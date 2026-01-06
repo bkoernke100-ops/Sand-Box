96ffbaff95e619559b3d785f6dfefe921c91b1e4
Brennen Koernke <bkoernke100@gmail.com>
2026-01-06 14:12:04 -0500

    Added variable with User Input

--- sandboc.py
+++ sandboc.py
@@ -9,3 +9,11 @@ Name = "Brennen Koernke"
 Age = 17
 
 print("My name is",Name,"it is the year",Year," and I am",Age,"years old!")
+
+print("")
+
+year = input("what year is it?: ")
+name = input("what is your name: ")
+age = input("how old are you: ")
+
+Print("\n Welcome",name,"I see you are",age,"years old in the year of",year + ".")
